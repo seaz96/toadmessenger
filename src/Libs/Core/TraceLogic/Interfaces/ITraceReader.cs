e9f4f13a561b342541c147ffea4411a3b4e3e90f
seaz96 <seaz442@mail.ru>
2024-03-18 17:40:45 +0500

    Return .sln to project

--- src/Libs/Core/TraceLogic/Interfaces/ITraceReader.cs
+++ src/Libs/Core/TraceLogic/Interfaces/ITraceReader.cs
@@ -1,10 +1,5 @@
 namespace Core.TraceLogic.Interfaces;
 
-/// <summary>
-/// чтение трассировочных значений при создании нового scoped
-///
-///  // для HTTP мы создаем middleware и  в нем это делаем 
-/// </summary>
 public interface ITraceReader
 {
     string Name { get; }
--- src/Libs/Core/TraceLogic/Interfaces/ITraceWriter.cs
+++ src/Libs/Core/TraceLogic/Interfaces/ITraceWriter.cs
@@ -1,8 +1,5 @@
 namespace Core.TraceLogic.Interfaces;
 
-/// <summary>
-/// Запись трассировочных значений при отправке запроса
-/// </summary>
 public interface ITraceWriter
 {
     string Name { get; }
